80bc32a9327f1c8d08ef10b567307895df5310a2
RageNeko26 <rneko2006@gmail.com>
2023-05-10 18:59:32 +0700

    Express Integration

--- index.ts
+++ index.ts
@@ -1,4 +1,10 @@
-const orm: any = require('sequelize');
+const express:any = require('express');
+const {Sequelize: orm, DataTypes: Types } = require('sequelize');
+
+
+
+const app: any = express();
+
 const initiate: any = new orm(
 	'sequelize',
 	'root',
@@ -8,8 +14,53 @@ const initiate: any = new orm(
 		dialect: 'mysql'
 	});
 
+const port: number = 5000;
+
+interface ResponseMessage {
+	message: string,
+	success: boolean,
+	status: number,
+}
+
+
 initiate.authenticate().then((): void => {
 	console.log('Connection to database is success!');
 }).catch((error: any) :void => {
 	console.log('Unable to connect to database!');
 })
+
+const BookTable = initiate.define('books', {
+	title: {
+		type: Types.STRING,
+		allowNull: false,
+	},
+	author: {
+		type: Types.STRING,
+		allowNull: false,
+	},
+	relase_date: {
+		type: Types.DATEONLY,
+	},
+	subject: {
+		type: Types.INTEGER
+	}
+})
+
+app.get('/', (req: any, res:any):void => {
+	let responseAPI: ResponseMessage = {
+		message: 'Hello world!',
+		success: true,
+		status: 200
+	}
+
+	res.end(JSON.stringify(responseAPI));
+});
+
+app.get('/books', async(req:any, res: any): Promise<void> => {
+	const query = await BookTable.findAll();
+	res.end(JSON.stringify(query, null, 2));
+})
+
+app.listen(port, (): void => {
+	console.log(`Server is running on port ${port}`);
+});
--- models/BookModel.ts
+++ models/BookModel.ts
@@ -8,13 +8,7 @@ const sequelize: any = new Sequelize(
 		dialect: 'mysql'
 	});
 
-
-sequelize.authenticate().then(():void => {
-	console.log('Connection to database is success!');
-}).catch((error: any) => {
-	console.log('Unable to connect to database!');
-})
-
+/* Define schema */
 const Book = sequelize.define('books', {
 	title: {
 		type: DataTypes.STRING,
@@ -33,26 +27,25 @@ const Book = sequelize.define('books', {
 })
 
 // With promise way
-sequelize.sync().then((): void => {
-	console.log('Book table was successfully created');
-	Book.findOne({
-		where: {
-			id: 1
-		}
-	}).then((res:any ) => {
-		console.log(`Filtered data by ID: ${JSON.stringify(res,null,2)}`);
-	}).catch((err:any) => {
-		console.log(err);
-	})
-}).catch((err:any) => {
-	console.log(`Failed to added table ${err}`);
-})
+// sequelize.sync().then((): void => {
+// 	console.log('Book table was successfully created');
+// 	Book.findOne({
+// 		where: {
+// 			id: 1
+// 		}
+// 	}).then((res:any ) => {
+// 		console.log(`Filtered data by ID: ${JSON.stringify(res,null,2)}`);
+// 	}).catch((err:any) => {
+// 		console.log(err);
+// 	})
+// }).catch((err:any) => {
+// 	console.log(`Failed to added table ${err}`);
+// })
 
 // With async await
 const retrieveData = async(): Promise<void> => {
 	const query = await Book.findAll();
 	console.log(`All data: ${JSON.stringify(query, null, 2)}`);
-	
 }
 
-retrieveData();
+retrieveData()
